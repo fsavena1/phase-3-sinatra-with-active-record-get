3c9bae5bcaf4f20e4e53c94f8b61878dcf65af88
frank <franksavena@gmail.com>
2022-12-26 09:13:30 -0600

    commit tree 6416211e36b98c4d515ab2422f52bcde7dcedc25

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, "application/json"
 
   get '/' do
     { message: "Hello world" }.to_json
+  end
+
+  get '/games/:id' do
+    # get all the games from the database
+    game = Game.find(params[:id])
+    # return a JSON response with an array of all the game data
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
